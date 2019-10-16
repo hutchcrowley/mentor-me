e664bb28fb4b5fe2c8a16c824d1d8bb44890a2f8
AustinLynes <austinlynes94@gmail.com>
2019-10-16 15:56:25 -0700

    added private route and axios with auth

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -4,8 +4,9 @@ export const axiosWithAuth = () => {
     const token = localStorage.getItem('token')
     return axios.create({
         baseURL: 'http:/localhost:5000',
-        headers:{
-            'Authorization':token
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `${token}`
         }
     })
 }
