f8abdb4663608f4bdda6bc5751d6740b02c187a8
AustinLynes <austinlynes94@gmail.com>
2019-10-16 18:38:14 -0700

    edit -- austin-lynes

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import { loginReducer } from './loginReducer'
 import { registerReducer } from './registerReducer'
+
 export const rootReducer = combineReducers({
     log: loginReducer,
     reg: registerReducer
