8694e3e9311bbe87f18c0d654b638718f18dbbea
AustinLynes <austinlynes94@gmail.com>
2019-10-18 19:43:49 -0700

    added preliminary registerReducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
+import axiosWithAuth from '../utils/axiosWithAuth'
 
+//#region MACROS
 export const LOG_IN_START = 'LOG_IN_START'
 export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS'
 export const LOG_IN_FAILED = 'LOG_IN_FAILED'
@@ -7,6 +9,13 @@ export const LOG_IN_FAILED = 'LOG_IN_FAILED'
 export const REGISTER_START = 'REGISTER_START'
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS'
 export const REGISTER_FAILED = 'REGISTER_FAILED'
+
+// export const FETCH_DATA_START = 'FETCH_DATA_START'
+// export const FETCH_DATA_SUCCESS = 'FETCH_DATA_START'
+// export const FETCH_DATA_FAILED = 'FETCH_DATA_START'
+//-- AUSTIN
+//#endregion 
+
 
 export const login = (creds) => dispatch => {
     dispatch({ type: LOG_IN_START })
@@ -37,3 +46,14 @@ export const register = (payload) => dispatch => {
             dispatch({ type: REGISTER_FAILED, payload: err })
         })
 }
+//QUESTION ACTION --- GET DATA FROM THE SERVER
+    //                         applyMiddleware(thunk)
+// export const getQuestions = () => dispatch => {
+//     dispatch({ type: FETCH_DATA_START })
+//     axiosWithAuth()
+//         .get('/api/questions')
+//         .then(res => {
+//             dispatch({ type: FETCH_DATA_SUCCESS, payload: res.data })
+//         })
+//         .catch(err => dispatch({type:FETCH_DATA_FAILED. payload:err})
+// }   AUSTIN -- 
