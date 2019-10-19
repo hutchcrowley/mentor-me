dff02dd66fdefcebb023c103f1ed686804149161
Just Me <lapaix360@hotmail.com>
2019-10-18 21:55:55 -0400

    added landing page and modify header to link back to home

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import Header from './header/Header';
-import '../styles/Styles.scss';
+import React from "react";
+import Header from "./header/Header";
+import "../styles/Styles.scss";
+import Landing from "./body/Landing";
+import RegisterUser from "./Register/RegisterUser";
+import { Route } from "react-router-dom";
 
 const App = () => {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+
+      <Route exact path="/" component={Landing} />
+      <Route path="/signup" component={RegisterUser} />
+    </div>
+  );
 };
 
 export default App;
-
