ca6891c6bd2dd8988ff8ab519e871711d45dcaac
AustinLynes <austinlynes94@gmail.com>
2019-10-18 17:55:01 -0700

    staging branch_austin-lynes

--- src/components/Register/RegisterUser.js
+++ src/components/Register/RegisterUser.js
@@ -1,77 +1,83 @@
-import React, {useState} from 'react';
-import axiosWithAuth from '../../utils/axiosWithAuth';
-import {connect} from 'react-redux'
-import {register} from '../../actions/index'
+import React, { useState } from 'react'
+import axiosWithAuth from '../../utils/axiosWithAuth'
+import { connect } from 'react-redux'
+import { register } from '../../actions/index'
 
 const RegisterUser = (props) => {
     const [payload, setPayload] = useState({
-        firstName: '',
-        lastName: '',
-        email: '',
-        password: '',
+        firstName:'',
+        lastName:'',
+        email:'',
+        password:'',
         isMentor: false
     })
-
     const handleMentor = e => {
+        let isActive = payload.isMentor
         setPayload({
             ...payload,
-            isMentor: !isMentor
+            isMentor: !isActive
         })
-        
+        console.log(`changing status master...  ${payload.isMentor.value}`)
     }
 
     const handleChange = (e) => {
         e.preventDefault()
         setPayload({
-            ...payload,
+            ...props.reg_user.state,
             [e.target.name]: e.target.value
         })
     }
-
+    
     const handleSubmit = e => {
-        e.preventDefault();
+        alert(JSON.stringify(payload))
+        console.log(JSON.stringify(payload))
+        e.preventDefault()
         props.register(payload)
     }
 
-    return(
+    return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input 
+                <input
                     type='text'
                     name='firstName'
                     placeholder='First Name'
                     value={payload.firstName}
-                    // onChange={handleChange}
+                    onChange={handleChange}
                 />
-                <input 
+                {/* {console.log(props.reg_user)} */}
+                <input
                     type='text'
                     name='lastName'
                     placeholder='Last Name'
                     value={payload.lastName}
-                    // onChange={handleChange}
+                    onChange={handleChange}
                 />
-                <input 
+                <input
                     type='email'
                     name='email'
                     placeholder='Email'
                     value={payload.email}
-                    // onChange={handleChange}
+                    onChange={handleChange}
                 />
-                <input 
+                <input
                     type='password'
                     name='password'
                     placeholder='Password'
                     value={payload.password}
-                    // onChange={handleChange}
+                    onChange={handleChange}
                 />
-                <input 
+                <label htmlFor='isMentor'>
+                    I am registering as mentor
+                </label>
+
+                <input
                     type='checkbox'
                     name='isMentor'
-                    onchange={handleMentor}
+                    onChange={handleMentor}
                 >
-                    I am registering as mentor
                 </input>
-                
+                <button type='submit'/>
             </form>
         </div>
     )
@@ -80,8 +86,8 @@ const RegisterUser = (props) => {
 
 const mapStateToProps = (state) => {
     return {
-        register = state.reg
+        reg_user: state.reg
     }
 }
 
-export default connect(mapStateToProps, {register})(RegisterUser)
+export default connect(mapStateToProps, { register })(RegisterUser)
