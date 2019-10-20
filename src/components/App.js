572edfe8745760a0ddfddd7b41711288a30ef6db
alphaseinor <alphaseinor@gmail.com>
2019-10-20 18:04:19 -0500

    added Login page and modified routes to access through Landing

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import React from "react";
-import Header from "./header/Header";
-import "../styles/Styles.scss";
-import Landing from "./body/Landing";
-import RegisterUser from "./Register/RegisterUser";
-import { Route } from "react-router-dom";
+import React from 'react';
+import Header from './header/Header';
+import '../styles/Styles.scss';
+import Landing from './body/Landing';
+import RegisterUser from './Register/RegisterUser';
+import Login from './body/login/Login';
+import { Route } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
 
       <Route exact path="/" component={Landing} />
       <Route path="/signup" component={RegisterUser} />
+      <Route path="/signin" component={Login} />
     </div>
   );
 };
