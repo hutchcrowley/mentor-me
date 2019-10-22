d439a9dcdb8971f89684cbf8da073c5fcb83a364
AustinLynes <austinlynes94@gmail.com>
2019-10-21 20:40:13 -0700

    trying to find upstream

--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -11,7 +11,7 @@ import { Route, Redirect } from 'react-router-dom'
 //         } />
 //     )
 // }
-const PrivateRoute = ({component: Component, ...rest})=>{
+const  PrivateRoute  = ({component: Component, ...rest})=>{
     return(
         <Route
         {...rest}
