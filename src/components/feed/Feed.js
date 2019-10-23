4044da3852e908e4731852c857265388371c88b2
alphaseinor <alphaseinor@gmail.com>
2019-10-22 21:22:11 -0500

    added call for questioncard

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,21 +1,30 @@
-import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
-import { getQuestions } from '../../actions'
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getQuestions } from '../../actions';
+import QuestionCard from './QuestionCard';
+
 const Feed = props => {
-    useEffect(() => {
-        props.getQuestions()
-    }, [])
-    return (
-        <div>
-            {props.questions ? console.log(JSON.stringify(props.questions)) : ''}
-        </div>
-    )
-}
-const mapStateToProp = (state) => {
-    return {
-        questions: state.quest.data
-    }
-}
-export default connect(mapStateToProp, {
+  useEffect(() => {
+    props.getQuestions();
+  }, []);
+  return (
+    <div>
+      {props.questions
+        ? props.questions.map(question => (
+            <QuestionCard key={question.id} question={question} />
+          ))
+        : ''}
+    </div>
+  );
+};
+const mapStateToProp = state => {
+  return {
+    questions: state.quest.data
+  };
+};
+export default connect(
+  mapStateToProp,
+  {
     getQuestions
-})(Feed)
+  }
+)(Feed);
